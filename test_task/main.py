0e58809223358489bdcbb596ff3fd231a38a0bed
ahzees <matviy.stets@gmail.com>
2024-05-07 19:14:30 +0300

    chore: created test for the project [Task]: test_task-6
    [Description list]
    feat: created tests for the project
    
    [Notes]
    <notes>

--- test_task/main.py
+++ test_task/main.py
@@ -1,35 +1,61 @@
+import asyncio
 import datetime
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 from conf import SAVE_DIR, images_logger, redis_client
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.responses import FileResponse
 from utils.image_processing import save_uploaded_files
 
 app = FastAPI()
 
 
+def process_uploaded_file(uploaded_file):
+    try:
+        return save_uploaded_files(uploaded_file)
+    except Exception as e:
+        images_logger.error(f"Error processing file {uploaded_file.filename}: {str(e)}")
+        raise HTTPException(status_code=500, detail="Failed to process the image")
+
+
+def get_cached_file_path(uploaded_file):
+    cached_file_path = redis_client.get(f"gray_{uploaded_file.filename}")
+    if cached_file_path:
+        cached_file_path = cached_file_path.decode()
+        if os.path.exists(cached_file_path):
+            images_logger.info(
+                f"Returning cached image for {uploaded_file.filename};\
+                time - {datetime.datetime.now()}"
+            )
+            return cached_file_path
+
+    return None
+
+
 @app.post("/convert/")
 async def convert_images(uploaded_file: UploadFile = File(...)):
-    """
-    Endpoint to handle image conversion.
-    """
-    # Create directory if it doesn't exist
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
 
-    # Save and convert the image
-    save_uploaded_files(uploaded_file)
+    cached_file_path = None
+    with ThreadPoolExecutor() as executor:
+        cached_file_path = executor.submit(get_cached_file_path, uploaded_file).result()
 
-    # Get the converted file path from Redis
-    cached_file = redis_client.get(f"gray_{uploaded_file.filename}")
-    if cached_file:
-        converted_file_path = cached_file.decode()
-    else:
-        return {"error": "Failed to convert the image"}
+    if cached_file_path:
+        return FileResponse(cached_file_path, media_type="image/jpeg")
+
+    loop = asyncio.get_event_loop()
+    gray_file_path = await loop.run_in_executor(
+        None, process_uploaded_file, uploaded_file
+    )
 
     images_logger.info(
-        f"Converted image - {converted_file_path}; time - {datetime.datetime.now()}"
+        f"Converted image - {gray_file_path}; time - {datetime.datetime.now()}"
     )
-    # Return the converted image as a response
-    return FileResponse(converted_file_path, media_type="image/jpeg")
+    return FileResponse(
+        gray_file_path,
+        media_type="image/jpeg"
+        if uploaded_file.content_type.startswith("image/jpeg")
+        else "image/png",
+    )
--- test_task/utils/image_processing.py
+++ test_task/utils/image_processing.py
@@ -5,22 +5,25 @@ from conf import SAVE_DIR, redis_client
 from fastapi import UploadFile
 
 
-def convert_to_gray(input_path: str, output_path: str) -> str:
+def convert_to_gray(input_path: str, output_path: str):
     image = cv2.imread(input_path)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(output_path, gray_image)
+    return output_path
 
 
 def save_uploaded_files(uploaded_file: UploadFile):
-    """
-    Function to save uploaded files to the specified directory.
-    """
-    file_content = uploaded_file.file.read()
     file_path = os.path.join(SAVE_DIR, uploaded_file.filename)
+    gray_file_path = os.path.join(SAVE_DIR, f"gray_{uploaded_file.filename}")
+
+    # Читання та збереження оригінального файлу
+    file_content = uploaded_file.file.read()
     with open(file_path, "wb") as buffer:
         buffer.write(file_content)
-    # Convert and save grayscale image
-    gray_file_path = os.path.join(SAVE_DIR, f"gray_{uploaded_file.filename}")
+
+    # Конвертація та збереження у відтінках сірого
     convert_to_gray(file_path, gray_file_path)
-    # Cache the grayscale image
+
+    # Кешування шляху до обробленого зображення
     redis_client.set(f"gray_{uploaded_file.filename}", gray_file_path)
+    return gray_file_path
